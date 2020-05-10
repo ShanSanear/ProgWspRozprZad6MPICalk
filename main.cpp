a858ea9dbd5ebed5dd99540e58ddfc75681ab2d6
ShanSanear <medan1993@gmail.com>
2020-05-10 20:20:31 +0200

    Added missing filling of struct

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
-int n = 1000;
+int n = 100000000;
 
 struct ToCalculate {
     double a;
@@ -18,7 +18,6 @@ double get_double_from_stdin(const char* message) {
     double out;
     std::cout << message << std::endl;
     std::cin >> out;
-    printf("Returning: %f\n", out);
     return out;
 }
 class IntegralCalculator {
@@ -95,14 +94,14 @@ int main(int argc, char *argv[])
     MPI_Datatype types[5] = {MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE};
     MPI_Aint displacements[5] = {0, sizeof(double), sizeof(double) * 2, sizeof(double) * 3,
     sizeof(double) * 4};
-    MPI_Datatype toCalculateMpiDatatype;
-    MPI_Type_create_struct(5, lengths, displacements, types, &toCalculateMpiDatatype);
-    MPI_Type_commit(&toCalculateMpiDatatype);
-    int node, Nnodes, tag = 2;
+    MPI_Datatype mpiDatatype;
+    MPI_Type_create_struct(5, lengths, displacements, types, &mpiDatatype);
+    MPI_Type_commit(&mpiDatatype);
+    int node, Nnodes;
+    int tag = 2;
     int finalized = 3;
-    double data = 0;
     int rec, recv;
-
+    double t1;
     MPI_Comm_rank(MPI_COMM_WORLD, &node);
     MPI_Comm_size(MPI_COMM_WORLD, &Nnodes);
     if (node == 0) {
@@ -111,12 +110,23 @@ int main(int argc, char *argv[])
         c = get_double_from_stdin("Specify c:");
         start = get_double_from_stdin("Specify start point:");
         end = get_double_from_stdin("Specify end point:");
+        struct ToCalculate singleNodeToCalculate;
+        singleNodeToCalculate.a = a;
+        singleNodeToCalculate.b = b;
+        singleNodeToCalculate.c = c;
+        singleNodeToCalculate.start = start;
+        singleNodeToCalculate.end = end;
+        IntegralCalculator singleNodeCalculator(singleNodeToCalculate);
+        t1 = MPI_Wtime();
+        singleNodeCalculator.calculate_trapezoidal_rule();
+        t1 = MPI_Wtime() - t1;
+
     }
     double delta = (end - start)/(Nnodes);
     double localSum = 0.0;
     double result = 0.0;
     MPI_Barrier(MPI_COMM_WORLD);
-    double time = MPI_Wtime();
+    double tp = MPI_Wtime();
     if (node == 0)
     {
         for (int i = 0; i < Nnodes; i++) {
@@ -128,23 +138,19 @@ int main(int argc, char *argv[])
             toBeCalculated.end = start + delta * (i+1);
             double i_st = start + delta * i;
             double i_end = start + delta * (i+1);
-            printf("Sending to: %d\n", i+1);
-            printf("%d / %d \n", node, Nnodes);
             if (i+1 != Nnodes) {
-            MPI_Send(&toBeCalculated, 1, toCalculateMpiDatatype, i+1, tag, MPI_COMM_WORLD);
+            MPI_Send(&toBeCalculated, 1, mpiDatatype, i+1, tag, MPI_COMM_WORLD);
             } else {
                 IntegralCalculator calc(toBeCalculated);
         localSum += calc.calculate_trapezoidal_rule();
             }
         }
-        printf("Ending with node 0\n");
     }
-    MPI_Barrier(MPI_COMM_WORLD);
-    if (node > 0)
+    else
     {
         printf("Node: %d\n", node);
         struct ToCalculate receivedCalculate;
-        MPI_Recv(&receivedCalculate, 1, toCalculateMpiDatatype, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&receivedCalculate, 1, mpiDatatype, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         IntegralCalculator calc(receivedCalculate);
         localSum += calc.calculate_trapezoidal_rule();
 
@@ -152,12 +158,13 @@ int main(int argc, char *argv[])
     MPI_Reduce(&localSum, &result, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
     if (node == 0) {
-        time = MPI_Wtime() - time;
-        printf("Calculating took: %f seconds\n", time);
+        tp = MPI_Wtime() - tp;
+        printf("Parallized calculation took: %f seconds\n", tp);
+        printf("Single node, calculation took: %f seconds\n", t1);
         printf("Result: %f\n", result);
     }
     
-    MPI_Type_free(&toCalculateMpiDatatype);
+    MPI_Type_free(&mpiDatatype);
     MPI_Finalize();
     return 0;
 }
