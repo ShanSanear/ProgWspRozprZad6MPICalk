ea248e7985d348cd24c72b3cb6b00177e604922e
ShanSanear <medan1993@gmail.com>
2020-05-13 17:16:10 +0200

    Complete overhaul of the script - without class.

--- main.cpp
+++ main.cpp
@@ -1,170 +1,153 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <mpi.h>
-#include <vector>
 #include <iostream>
-#include <cmath>
-int n = 100000000;
+#include <iomanip>
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <stdio.h>
+#include <ctime>
+#include <mpi.h>
 
-struct ToCalculate {
-    double a;
-    double b;
-    double c;
-    double start;
-    double end;
+using namespace std;
+double A, B, C;
+int N;
+double startTime, endTime, parallelTimeTaken, timeSingle;
+
+struct CalculateParameters
+{
+    double A;
+    double B;
+    double C;
 };
 
-double get_double_from_stdin(const char* message) {
+double calculate_formula(double x)
+{
+    return (A * (x * x) + B * x + C);
+}
+
+double get_double_from_stdin(const char *message)
+{
     double out;
     std::cout << message << std::endl;
     std::cin >> out;
     return out;
 }
-class IntegralCalculator {
-private:
-    double a, b, c;
-    double starting_point, ending_point;
-    double dx;
-    
-    double calculate_formula(double x) const;
 
-public:
-    IntegralCalculator(ToCalculate toBeCalculated);
-
-    double calculate_rectangular_method();
-
-    double calculate_trapezoidal_rule();
-
-};
-
-
-IntegralCalculator::IntegralCalculator(ToCalculate toBeCalculated) {
-    this->a = toBeCalculated.a;
-    this->b = toBeCalculated.b;
-    this->c = toBeCalculated.c;
-    this->starting_point = toBeCalculated.start;
-    this->ending_point = toBeCalculated.end;
-    this->dx = (ending_point - starting_point) / n;
-}
-
-double IntegralCalculator::calculate_formula(double x) const {
-    return a * powl(x, 2) + b * x + c;
-}
-
-double IntegralCalculator::calculate_rectangular_method() {
-    printf("Calculating using rectangular method\n");
-    double calculated_integral = 0.0;
-    
-    for (int i = 1; i < n; i++) {
-        calculated_integral += calculate_formula(starting_point + i * dx) * dx;
-    }
-    
-    
-    printf("Calculated integral for limits from %f to %f is: %.15f\n", starting_point, ending_point,
-           calculated_integral);
-
-    return calculated_integral;
-}
-
-double IntegralCalculator::calculate_trapezoidal_rule() {
-    printf("Calculating using trapezoidal rule\n");
-    double calculated_integral = 0.0;
-    
-    double value_for_start = calculate_formula(starting_point);
-    double value_for_end = calculate_formula(ending_point);
-    double average = (value_for_start + value_for_end) / 2;
-    for (int i = 1; i < n; i++) {
-        calculated_integral += (calculate_formula(starting_point + (i * dx)));
-    }
-    calculated_integral += average;
-    calculated_integral *= dx;
-    
-    
-    printf("Calculated integral for limits from %f to %f is: %.15f\n", starting_point, ending_point,
-           calculated_integral);
-    return calculated_integral;
-}
-
-int main(int argc, char *argv[])
+int get_int_from_stdin(const char *message)
 {
-    double a,b,c,start,end;
-    
-    MPI_Init(&argc, &argv);
-    int lengths[5] = {1,1,1,1,1};
-    MPI_Datatype types[5] = {MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE};
-    MPI_Aint displacements[5] = {0, sizeof(double), sizeof(double) * 2, sizeof(double) * 3,
-    sizeof(double) * 4};
-    MPI_Datatype mpiDatatype;
-    MPI_Type_create_struct(5, lengths, displacements, types, &mpiDatatype);
-    MPI_Type_commit(&mpiDatatype);
-    int node, Nnodes;
-    int tag = 2;
-    int finalized = 3;
-    int rec, recv;
-    double t1;
-    MPI_Comm_rank(MPI_COMM_WORLD, &node);
-    MPI_Comm_size(MPI_COMM_WORLD, &Nnodes);
-    if (node == 0) {
-        a = get_double_from_stdin("Specify a:");
-        b = get_double_from_stdin("Specify b:");
-        c = get_double_from_stdin("Specify c:");
-        start = get_double_from_stdin("Specify start point:");
-        end = get_double_from_stdin("Specify end point:");
-        struct ToCalculate singleNodeToCalculate;
-        singleNodeToCalculate.a = a;
-        singleNodeToCalculate.b = b;
-        singleNodeToCalculate.c = c;
-        singleNodeToCalculate.start = start;
-        singleNodeToCalculate.end = end;
-        IntegralCalculator singleNodeCalculator(singleNodeToCalculate);
-        t1 = MPI_Wtime();
-        singleNodeCalculator.calculate_trapezoidal_rule();
-        t1 = MPI_Wtime() - t1;
+    int out;
+    std::cout << message << std::endl;
+    std::cin >> out;
+    return out;
+}
 
-    }
-    double delta = (end - start)/(Nnodes);
-    double localSum = 0.0;
-    double result = 0.0;
-    MPI_Barrier(MPI_COMM_WORLD);
-    double tp = MPI_Wtime();
+int main()
+{
+    double sum, dx, start_point, end_point, result;
+    double functionParameters[3] = {0};
+    double functionLimits[2] = {0};
+
+    int node, numOfNodes;
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &numOfNodes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &node);
+    cout << setprecision(3) << fixed;
+    int lengths[4] = {1, 1, 1};
+    MPI_Datatype types[3] = {MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE};
+    MPI_Aint displacements[3] = {0, sizeof(double), sizeof(double) * 2};
+
+    MPI_Datatype mpiCalculateParametersDatatype;
+    MPI_Type_create_struct(3, lengths, displacements, types, &mpiCalculateParametersDatatype);
+    MPI_Type_commit(&mpiCalculateParametersDatatype);
+
     if (node == 0)
     {
-        for (int i = 0; i < Nnodes; i++) {
-            struct ToCalculate toBeCalculated;
-            toBeCalculated.a = a;
-            toBeCalculated.b = b;
-            toBeCalculated.c = c;
-            toBeCalculated.start = start + delta * i;
-            toBeCalculated.end = start + delta * (i+1);
-            double i_st = start + delta * i;
-            double i_end = start + delta * (i+1);
-            if (i+1 != Nnodes) {
-            MPI_Send(&toBeCalculated, 1, mpiDatatype, i+1, tag, MPI_COMM_WORLD);
-            } else {
-                IntegralCalculator calc(toBeCalculated);
-        localSum += calc.calculate_trapezoidal_rule();
-            }
+
+        A = get_double_from_stdin("Specify a:");
+        B = get_double_from_stdin("Specify b:");
+        C = get_double_from_stdin("Specify c:");
+        N = get_int_from_stdin("Specifiy N:");
+        start_point = get_double_from_stdin("Specify start point:");
+        end_point = get_double_from_stdin("Specify end point:");
+
+        sum = 0;
+        dx = (end_point - start_point) / N;
+
+        startTime = MPI_Wtime();
+        for (int i = 0; i <= N; i++)
+        {
+            sum += calculate_formula(start_point + i * dx);
         }
+
+        sum *= dx;
+
+        endTime = MPI_Wtime();
+
+        timeSingle = endTime - startTime;
+        printf("Single node result: %f\n", sum);
+        printf("Single node time: %f second\n", timeSingle);
+        struct CalculateParameters calculateStruct;
+        //There is no MPI_Barrier here because only one node can reach this section
+        startTime = MPI_Wtime();
+        functionLimits[0] = start_point;
+        functionLimits[1] = end_point;
+        calculateStruct.A = A;
+        calculateStruct.B = B;
+        calculateStruct.C = C;
+        for (int i = 1; i < numOfNodes; i++)
+        {
+            MPI_Send(&calculateStruct, 1, mpiCalculateParametersDatatype, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&N, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(functionLimits, 2, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+        }
+
+        sum = 0;
+        dx = (end_point - start_point) / N;
+
+        for (int i = 0; i <= N / numOfNodes; i++)
+        {
+            sum += calculate_formula(start_point + i * dx);
+        }
+
+        sum *= dx;
     }
     else
     {
-        printf("Node: %d\n", node);
-        struct ToCalculate receivedCalculate;
-        MPI_Recv(&receivedCalculate, 1, mpiDatatype, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        IntegralCalculator calc(receivedCalculate);
-        localSum += calc.calculate_trapezoidal_rule();
+        struct CalculateParameters calcStruct;
+        // MPI_Recv(functionParameters, 3, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&calcStruct, 1, mpiCalculateParametersDatatype, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        // printf("What is inside? %f, %f, %f, %d\n", calcStruct.A, calcStruct.B, calcStruct.C, calcStruct.N);
+        MPI_Recv(&N, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(functionLimits, 2, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
+        start_point = functionLimits[0];
+        end_point = functionLimits[1];
+        A = calcStruct.A;
+        B = calcStruct.B;
+        C = calcStruct.C;
+
+        sum = 0;
+        dx = (end_point - start_point) / N;
+
+        for (int i = node * (N / numOfNodes); i <= node * N / numOfNodes + N / numOfNodes; i++)
+        {
+            sum += calculate_formula(start_point + i * dx);
+        }
+        int check = ((node * (N / numOfNodes)) + (N / numOfNodes));
+        int check2 = node * N / numOfNodes + N / numOfNodes;
+        printf("Check 1: %d Check 2: %d\n", check, check2);
+        sum *= dx;
     }
-    MPI_Reduce(&localSum, &result, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    MPI_Reduce(&sum, &result, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
-    if (node == 0) {
-        tp = MPI_Wtime() - tp;
-        printf("Parallized calculation took: %f seconds\n", tp);
-        printf("Single node, calculation took: %f seconds\n", t1);
-        printf("Result: %f\n", result);
+    if (node == 0)
+    {
+        endTime = MPI_Wtime();
+        parallelTimeTaken = endTime - startTime;
+        printf("Parallized result: %f\n", result);
+        printf("Parallized time: %f seconds\n", parallelTimeTaken);
     }
-    
-    MPI_Type_free(&mpiDatatype);
+    MPI_Type_free(&mpiCalculateParametersDatatype);
     MPI_Finalize();
     return 0;
 }
