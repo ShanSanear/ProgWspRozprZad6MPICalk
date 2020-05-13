6e5c89f7fa9b0addd737192036422d4dc13ef60a
ShanSanear <medan1993@gmail.com>
2020-05-13 19:30:44 +0200

    Added log library usage

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <iomanip>
 #include <iostream>
-#include <cstdlib>
 #include <cstdio>
 #include <stdio.h>
 #include <ctime>
 #include <mpi.h>
+#include <plog/Log.h>
+#include <plog/Appenders/ConsoleAppender.h>
 
 using namespace std;
 double A, B, C;
@@ -45,7 +46,9 @@ int main()
     double sum, dx, start_point, end_point, result;
     double functionParameters[3] = {0};
     double functionLimits[2] = {0};
-
+    plog::RollingFileAppender<plog::CsvFormatter> fileAppender("Datalogger.log", 1048576, 5);
+    plog::ConsoleAppender<plog::TxtFormatter> consoleAppender;
+    plog::init(plog::info, &fileAppender).addAppender(&consoleAppender);
     int node, numOfNodes;
     MPI_Init(NULL, NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &numOfNodes);
@@ -61,7 +64,7 @@ int main()
 
     if (node == 0)
     {
-
+        PLOG_INFO << "Getting input values";
         A = get_double_from_stdin("Specify a:");
         B = get_double_from_stdin("Specify b:");
         C = get_double_from_stdin("Specify c:");
@@ -73,6 +76,7 @@ int main()
         dx = (end_point - start_point) / N;
 
         startTime = MPI_Wtime();
+        PLOG_INFO << "Processing on single node";
         for (int i = 0; i <= N; i++)
         {
             sum += calculate_formula(start_point + i * dx);
@@ -83,8 +87,8 @@ int main()
         endTime = MPI_Wtime();
 
         timeSingle = endTime - startTime;
-        printf("Single node result: %f\n", sum);
-        printf("Single node time: %f second\n", timeSingle);
+        PLOG_INFO << "Single node result: " << sum;
+        PLOG_INFO << "Single node time: " << timeSingle << " second(s)";
         struct CalculateParameters calculateStruct;
         //There is no MPI_Barrier here because only one node can reach this section
         startTime = MPI_Wtime();
@@ -93,6 +97,7 @@ int main()
         calculateStruct.A = A;
         calculateStruct.B = B;
         calculateStruct.C = C;
+        PLOG_INFO << "Sending data to other nodes";
         for (int i = 1; i < numOfNodes; i++)
         {
             MPI_Send(&calculateStruct, 1, mpiCalculateParametersDatatype, i, 0, MPI_COMM_WORLD);
@@ -102,7 +107,7 @@ int main()
 
         sum = 0;
         dx = (end_point - start_point) / N;
-
+        PLOG_INFO << "Processing data on node 0";
         for (int i = 0; i <= N / numOfNodes; i++)
         {
             sum += calculate_formula(start_point + i * dx);
@@ -113,12 +118,10 @@ int main()
     else
     {
         struct CalculateParameters calcStruct;
-        // MPI_Recv(functionParameters, 3, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         MPI_Recv(&calcStruct, 1, mpiCalculateParametersDatatype, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        // printf("What is inside? %f, %f, %f, %d\n", calcStruct.A, calcStruct.B, calcStruct.C, calcStruct.N);
         MPI_Recv(&N, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         MPI_Recv(functionLimits, 2, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
+        PLOG_INFO << "Processing data, node: " << node;
         start_point = functionLimits[0];
         end_point = functionLimits[1];
         A = calcStruct.A;
@@ -132,11 +135,9 @@ int main()
         {
             sum += calculate_formula(start_point + i * dx);
         }
-        int check = ((node * (N / numOfNodes)) + (N / numOfNodes));
-        int check2 = node * N / numOfNodes + N / numOfNodes;
-        printf("Check 1: %d Check 2: %d\n", check, check2);
         sum *= dx;
     }
+    PLOG_INFO << "Finished processing, node: " << node;
 
     MPI_Reduce(&sum, &result, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
@@ -144,8 +145,8 @@ int main()
     {
         endTime = MPI_Wtime();
         parallelTimeTaken = endTime - startTime;
-        printf("Parallized result: %f\n", result);
-        printf("Parallized time: %f seconds\n", parallelTimeTaken);
+        PLOG_INFO << "Parallized result: " << result;
+        PLOG_INFO << "Parallized time: " << parallelTimeTaken << " second(s)";
     }
     MPI_Type_free(&mpiCalculateParametersDatatype);
     MPI_Finalize();
